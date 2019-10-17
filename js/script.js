4e0e9d754d5887e0241b00bb1bdbcd7d76b1732d
honglihongli <s_hong.li@ousd.org>
2019-10-17 01:28:11 +0000

    huh

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 $( "document" ).ready(function() {
+$("#button").click(function() {
+    var go = $(".input").val();
+    var nextinput1 = go.indexOf("a");
+    let one1 = go.slice(nextinput1);
+    let one2 = go.slice(0,one);
+    let two = one1+one2+"ay";
+alert(two);
 
 
-
+    $(".output").text(two);
+});
 });
